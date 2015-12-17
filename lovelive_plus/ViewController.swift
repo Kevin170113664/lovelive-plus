5e40bc121705f91eef0bd7cc7e614effb7c9da6e
Li Linhao <lhli@thoughtworks.com>
2015-12-17 18:45:24 +0800

    Initial Commit

---
+++ lovelive_plus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lovelive_plus
+//
+//  Created by Linhao Li on 12/17/15.
+//  Copyright © 2015 Kevin Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
