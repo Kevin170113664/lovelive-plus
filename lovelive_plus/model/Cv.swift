e97918906aa5f1179f73aaa6d5002a29738f4e6c
Kevin170113664 <lhli@thoughtwork.com>
2015-12-24 15:54:08 +0800

    Added fields in idol and cv model.

--- lovelive_plus/model/Cv.swift
+++ lovelive_plus/model/Cv.swift
@@ -1,4 +1,17 @@
 import Foundation
 
 class Cv {
+    var url: String
+    var twitter: String
+    var nickname: String
+    var name: String
+    var instagram: String
+
+    init(url: String, twitter: String, nickname: String, name: String, instagram: String) {
+        self.url = url
+        self.twitter = twitter
+        self.nickname = nickname
+        self.name = name
+        self.instagram = instagram
+    }
 }
