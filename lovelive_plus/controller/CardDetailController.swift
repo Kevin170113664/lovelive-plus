c5ee20eadfba794297baa85190369e85c35250fe
Kevin170113664 <lhli@thoughtwork.com>
2016-01-14 14:53:48 +0800

    Added shadow for card detail view.

--- lovelive_plus/controller/CardDetailController.swift
+++ lovelive_plus/controller/CardDetailController.swift
@@ -2,8 +2,18 @@ import UIKit
 
 class CardDetailController: UIViewController {
     @IBOutlet var cardDetailView: UIView!
+    @IBOutlet weak var cardDetailScrollView: UIScrollView!
+    @IBOutlet weak var cardNonIdolizedImageButton: UIButton!
+    @IBOutlet weak var cardIdolizedImageButton: UIButton!
+    @IBOutlet weak var cardPanelView: UIView!
 
     override func viewDidLoad() {
         cardDetailView.backgroundColor = Color.Blue50()
+        
+        cardPanelView.backgroundColor = Color.Blue100()
+        cardPanelView!.layer.shadowOffset = CGSizeMake(1, 1)
+        cardPanelView!.layer.shadowRadius = 5.0
+        cardPanelView!.layer.shadowOpacity = 0.8
+        cardPanelView!.layer.masksToBounds = false
     }
 }
