d8feaa776d49b3bc8b0bcfe2619b63f0b3406f17
Kevin170113664 <lhli@thoughtwork.com>
2016-01-14 16:43:34 +0800

    Added skill panel for card detail page.

--- lovelive_plus/controller/CardDetailController.swift
+++ lovelive_plus/controller/CardDetailController.swift
@@ -6,7 +6,8 @@ class CardDetailController: UIViewController {
     @IBOutlet weak var cardNonIdolizedImageButton: UIButton!
     @IBOutlet weak var cardIdolizedImageButton: UIButton!
     @IBOutlet weak var cardPanelView: UIView!
-
+    @IBOutlet weak var cardSkillPanelView: UIView!
+    
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var idlabel: UILabel!
     @IBOutlet weak var minSmile: UILabel!
@@ -19,13 +20,24 @@ class CardDetailController: UIViewController {
     @IBOutlet weak var idolizedMaxPure: UILabel!
     @IBOutlet weak var idolizedMaxCool: UILabel!
     
+    @IBOutlet weak var skillType: UILabel!
+    @IBOutlet weak var releaseDate: UILabel!
+    @IBOutlet weak var centerSkillName: UILabel!
+    @IBOutlet weak var centerSkillDetail: UILabel!
+    @IBOutlet weak var skillName: UILabel!
+    @IBOutlet weak var skillDetail: UILabel!
+    
     override func viewDidLoad() {
         cardDetailView.backgroundColor = Color.Blue50()
-        
-        cardPanelView.backgroundColor = Color.Blue100()
-        cardPanelView!.layer.shadowOffset = CGSizeMake(1, 1)
-        cardPanelView!.layer.shadowRadius = 5.0
-        cardPanelView!.layer.shadowOpacity = 0.8
-        cardPanelView!.layer.masksToBounds = false
+        setShadowForView(cardPanelView)
+        setShadowForView(cardSkillPanelView)
+    }
+
+    func setShadowForView(view: UIView) {
+        view.backgroundColor = Color.Blue100()
+        view.layer.shadowOffset = CGSizeMake(1, 1)
+        view.layer.shadowRadius = 5.0
+        view.layer.shadowOpacity = 0.8
+        view.layer.masksToBounds = false
     }
 }
