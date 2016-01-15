0b63245394b2ea999955e2e9cd906e0e8156247b
Kevin170113664 <lhli@thoughtwork.com>
2016-01-15 16:19:06 +0800

    Set content for card detail page.

--- lovelive_plus/controller/CardDetailController.swift
+++ lovelive_plus/controller/CardDetailController.swift
@@ -3,14 +3,14 @@ import UIKit
 class CardDetailController: UIViewController {
     internal var cardId: String?
     internal var card: Card?
-    
+
     @IBOutlet var cardDetailView: UIView!
     @IBOutlet weak var cardDetailScrollView: UIScrollView!
     @IBOutlet weak var cardNonIdolizedImageButton: UIButton!
     @IBOutlet weak var cardIdolizedImageButton: UIButton!
     @IBOutlet weak var cardPanelView: UIView!
     @IBOutlet weak var cardSkillPanelView: UIView!
-    
+
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var idlabel: UILabel!
     @IBOutlet weak var minSmile: UILabel!
@@ -22,27 +22,73 @@ class CardDetailController: UIViewController {
     @IBOutlet weak var idolizedMaxSmile: UILabel!
     @IBOutlet weak var idolizedMaxPure: UILabel!
     @IBOutlet weak var idolizedMaxCool: UILabel!
-    
+
     @IBOutlet weak var skillType: UILabel!
     @IBOutlet weak var releaseDate: UILabel!
     @IBOutlet weak var centerSkillName: UILabel!
     @IBOutlet weak var centerSkillDetail: UILabel!
     @IBOutlet weak var skillName: UILabel!
     @IBOutlet weak var skillDetail: UILabel!
-    
+
     override func viewDidLoad() {
         cardDetailView.backgroundColor = Color.Blue50()
         setShadowForView(cardPanelView)
         setShadowForView(cardSkillPanelView)
-        idlabel.text = cardId ?? "Unknown"
-//        setCardDetail()
+        setCardDetail()
     }
-    
+
     func setCardDetail() {
-        cardNonIdolizedImageButton.imageView?.sd_setImageWithURL(NSURL(string: card!.cardImage!))
-        cardIdolizedImageButton.imageView?.sd_setImageWithURL(NSURL(string: card!.cardIdolizedImage!))
-        nameLabel.text = card!.name ?? "Unknown"
-        idlabel.text = card!.cardId ?? "Unknown"
+        card = DataController().queryCardById(cardId!)
+
+        setCardImage()
+        setBasicInfo()
+        setSkillInfo()
+    }
+
+    func nonIdolizedImageExist() -> Bool {
+        return card?.isPromo == 0 && card?.isSpecial == 0 && card!.cardImage != nil && card!.cardImage != ""
+    }
+
+    func setCardImage() {
+        if (nonIdolizedImageExist()) {
+            cardNonIdolizedImageButton.sd_setBackgroundImageWithURL(NSURL(string: card!.cardImage!), forState: UIControlState.Normal)
+        }
+        cardIdolizedImageButton.sd_setBackgroundImageWithURL(NSURL(string: card!.cardIdolizedImage!), forState: UIControlState.Normal)
+    }
+
+    func setBasicInfo() {
+        setLabelText(nameLabel, value: card!.japaneseName)
+        setLabelText(idlabel, value: card!.cardId)
+        setLabelText(minSmile, value: card!.minimumStatisticsSmile)
+        setLabelText(minPure, value: card!.minimumStatisticsPure)
+        setLabelText(minCool, value: card!.minimumStatisticsCool)
+        setLabelText(nonIdolizedMaxSmile, value: card!.nonIdolizedMaximumStatisticsSmile)
+        setLabelText(nonIdolizedMaxPure, value: card!.nonIdolizedMaximumStatisticsPure)
+        setLabelText(nonIdolizedMaxCool, value: card!.nonIdolizedMaximumStatisticsCool)
+        setLabelText(idolizedMaxSmile, value: card!.idolizedMaximumStatisticsSmile)
+        setLabelText(idolizedMaxPure, value: card!.idolizedMaximumStatisticsPure)
+        setLabelText(idolizedMaxCool, value: card!.idolizedMaximumStatisticsCool)
+    }
+
+    func setSkillInfo() {
+        setLabelText(skillType, value: card!.skill)
+        setLabelText(releaseDate, value: card!.releaseDate)
+        setLabelText(centerSkillName, value: isStringValid(card!.japaneseCenterSkill) ? card!.japaneseCenterSkill : card!.centerSkill)
+        setLabelText(centerSkillDetail, value: card!.japaneseCenterSkillDetails)
+        setLabelText(skillName, value: card!.japaneseSkill)
+        setLabelText(skillDetail, value: isStringValid(card!.japaneseSkillDetails) ? card!.japaneseSkillDetails : card!.skillDetails)
+    }
+
+    func setLabelText(label: UILabel, value: String?) {
+        if isStringValid(value) {
+            label.text = value
+        } else {
+            return
+        }
+    }
+
+    func isStringValid(value: String?) -> Bool {
+        return value != "" && value != nil
     }
 
     func setShadowForView(view: UIView) {
