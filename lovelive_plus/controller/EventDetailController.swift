c055c89e1823f4e3a53c81da1c4e1d0d10ee5a4f
Li Linhao <lhli@thoughtworks.com>
2016-03-27 14:34:43 +0800

    Finish show event detail page.

--- lovelive_plus/controller/EventDetailController.swift
+++ lovelive_plus/controller/EventDetailController.swift
@@ -2,6 +2,7 @@ import UIKit
 
 class EventDetailController: UIViewController {
     
+    @IBOutlet weak var eventDetailView: UIView!
     @IBOutlet weak var eventImage: UIImageView!
     @IBOutlet weak var eventTitle: UILabel!
     @IBOutlet weak var eventBeginTime: UILabel!
@@ -12,9 +13,46 @@ class EventDetailController: UIViewController {
     @IBOutlet weak var points2: UILabel!
 
     internal var eventName : String?
+    internal var event : Event?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(eventName)
+        
+        self.view.backgroundColor = Color.Blue50()
+        setShadowForView(eventDetailView)
+        setEventDetail()
+    }
+    
+    func setEventDetail() {
+        if let event = EventManager().queryEventByName(eventName) {
+            showEventImage(event)
+            eventTitle.text = eventName
+            eventBeginTime.text = textForLabel(event.beginning)
+            eventEndTime.text = textForLabel(event.end)
+            rank1.text = textForLabel(event.japaneseT1Rank)
+            points1.text = textForLabel(event.japaneseT1Points)
+            rank2.text = textForLabel(event.japaneseT2Rank)
+            points2.text = textForLabel(event.japaneseT2Points)
+        }
+    }
+
+    func showEventImage(event: Event!) {
+        if let eventImageUrl = event.image {
+            let encodedUrl = eventImageUrl.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())
+            eventImage.contentMode = UIViewContentMode.ScaleAspectFit
+            eventImage.sd_setImageWithURL(NSURL(string: encodedUrl!))
+        }
+    }
+
+    func textForLabel(rawText: AnyObject?) -> String! {
+        return rawText != nil ? String(rawText!) : "未知"
+    }
+    
+    func setShadowForView(view: UIView) {
+        view.backgroundColor = Color.Blue100()
+        view.layer.shadowOffset = CGSizeMake(1, 1)
+        view.layer.shadowRadius = 5.0
+        view.layer.shadowOpacity = 0.8
+        view.layer.masksToBounds = false
     }
 }
--- lovelive_plus/manager/EventManager.swift
+++ lovelive_plus/manager/EventManager.swift
@@ -4,7 +4,7 @@ import CoreData
 
 class EventManager: NSObject {
     var managedObjectContext: NSManagedObjectContext
-    
+
     override init() {
         guard let modelURL = NSBundle.mainBundle().URLForResource("CardModel", withExtension: "momd") else {
             fatalError("Error loading model from bundle")
@@ -12,22 +12,22 @@ class EventManager: NSObject {
         guard let mom = NSManagedObjectModel(contentsOfURL: modelURL) else {
             fatalError("Error initializing mom from: \(modelURL)")
         }
-        
+
         let psc = NSPersistentStoreCoordinator(managedObjectModel: mom)
         self.managedObjectContext = NSManagedObjectContext(concurrencyType: .MainQueueConcurrencyType)
         self.managedObjectContext.persistentStoreCoordinator = psc
-        
+
         let urls = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
         let docURL = urls[urls.endIndex - 1]
         let storeURL = docURL.URLByAppendingPathComponent("DataModel.sqlite")
-        
+
         do {
             try psc.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: storeURL, options: nil)
         } catch {
             fatalError("Error migrating store: \(error)")
         }
     }
-    
+
     func queryAllEvents() -> [Event] {
         let eventFetch = NSFetchRequest(entityName: "Event")
         eventFetch.returnsObjectsAsFaults = false
@@ -37,6 +37,22 @@ class EventManager: NSObject {
             } else {
                 return []
             }
+        } catch {
+            fatalError("Failed to fetch cards: \(error)")
+        }
+    }
+
+    func queryEventByName(eventName: String!) -> Event? {
+        let eventFetch = NSFetchRequest(entityName: "Event")
+        eventFetch.predicate = NSPredicate(format: "japaneseName = %@", eventName)
+        eventFetch.returnsObjectsAsFaults = false
+        do {
+            if let eventList = try self.managedObjectContext.executeFetchRequest(eventFetch) as? [Event] {
+                if eventList.count != 0 {
+                    return eventList[0]
+                }
+            }
+            return nil
         } catch {
             fatalError("Failed to fetch cards: \(error)")
         }
