a0f6b24b3b000e415ea6b85ce03200c1e390c967
Li Linhao <lhli@thoughtworks.com>
2016-07-12 20:53:32 +0800

    cannot make the fucking test runnable

---
+++ lovelive_plusTests/service/CardServiceTests.swift
@@ -0,0 +1,12 @@
+import Quick
+import Nimble
+
+class CardServiceTests: QuickSpec {
+	override func spec() {
+		describe("what the fuck") {
+			it("should be my first test") {
+				expect("Hello").notTo(equal(true))
+			}
+		}
+	}
+}
